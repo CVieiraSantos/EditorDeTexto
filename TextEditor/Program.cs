b53d5108248d6bb27a7da57ad4e48acbd745d2b4
Carlos Vieira <cadu.vieira2276@gmail.com>
2022-04-23 17:32:28 -0300

    Editor de textos em Console para praticar controle de fluxo, ler e gravar arquivos e processar inputs do teclado

---
+++ TextEditor/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1- Abrir arquivo");
+            Console.WriteLine("2- Criar novo arquivo");
+            Console.WriteLine("0-Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+
+        }
+
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                string[] vect = new string[1];
+                vect[0] = text;
+                Console.WriteLine(vect[0]);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+            Console.WriteLine("-----------------------");
+            string text = "";
+            do
+            {
+                text+= Console.ReadLine();
+                text+= Environment.NewLine;
+                
+            }
+
+            while(Console.ReadKey().Key != ConsoleKey.Escape); //Enquanto o caractere que o usuário digitar for diferente de ESC
+
+            Salvar(text);
+
+
+        }
+
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo?");
+            string path = Console.ReadLine();
+                        
+            
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);                
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso");
+            Console.ReadLine();
+            Menu();
+        }
+    }
+}
